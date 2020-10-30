646d7194f7e58e2a83f280c52437f041f4ef732e
Crowdbotics <team@crowdbotics.com>
2020-10-30 04:03:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nvisia_connect_22113/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nvisia_connect_22113;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "nvisia_connect_22113";
+    }
+}
